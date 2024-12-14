da3ee4423b0f5e838b540dd2909207b06852af1f
Akhil <akhilas.singh@gmail.com>
2024-12-14 18:18:53 +0530

    added readme

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("enter a number plz ...");
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read new line");
+
+    println!("You have guessed -> {}", guess);
 }
